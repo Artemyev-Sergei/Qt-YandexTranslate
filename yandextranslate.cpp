31eaf6a2a9a0ce549ee760829bf2a514cbf42c4b
Sergey Artemyev <artemyev-sergei@ya.ru>
2017-02-01 22:24:29 +0300

    Add raw Dictionary API support

--- yandextranslate.cpp
+++ yandextranslate.cpp
@@ -23,12 +23,14 @@ YandexTranslate::YandexTranslate(QWidget *parent) :
     ui->setupUi(this);
     this->setFixedSize(this->geometry().width(),this->geometry().height());
     this->getSupportedLanguages();
+    this->getTranslationDirections();
 
     ui->comboBoxSource->setCurrentText("English");
     ui->comboBoxTarget->setCurrentText("Russian");
 
     connect(ui->detectButton,SIGNAL(clicked(bool)),this,SLOT(detectLanguage()));
     connect(ui->translateButton,SIGNAL(clicked(bool)),this,SLOT(translate()));
+    connect(ui->dictionaryButton,SIGNAL(clicked(bool)),this,SLOT(getDictionaryEntry()));
 }
 
 YandexTranslate::~YandexTranslate()
@@ -40,9 +42,8 @@ void YandexTranslate::getSupportedLanguages()
 {
     // Create a request.
     QUrl url(QString("https://translate.yandex.net/api/v1.5/tr.json/getLangs?")
-             + QString("key=") + api_key
+             + QString("key=") + trnsl_api_key
              + QString("&ui=") + QString("en")
-             //+ QString("$callback=") + <name of the callback function>
              );
 
     QNetworkRequest request(url);
@@ -61,6 +62,8 @@ void YandexTranslate::getSupportedLanguages()
     QByteArray bytes = reply->readAll();
     QJsonObject obj = QJsonDocument::fromJson(bytes).object();  // Keys are ("dirs", "langs")
 
+    // qDegub() << obj.keys();  // Use it to check the keys.
+
     // Check the response codes.
     if(obj.contains("code") || obj.contains("message"))
     {
@@ -69,6 +72,13 @@ void YandexTranslate::getSupportedLanguages()
     }
 
     QJsonObject langs = obj.value("langs").toObject();  // Codes and Languages.
+
+    /* Use the following commented line to check the type of an object.
+     * There're also such functions as isArray(), isDouble(), etc.
+     * See Qt Documentation for more examples.
+     */
+    //qDebug() << obj.value("langs").isObject();
+
     for(const QString& code : langs.keys())
     {
         QString lang = langs.value(code).toString();
@@ -84,10 +94,9 @@ void YandexTranslate::detectLanguage()
     // Create a request.
     QString source_text = ui->SourceEdit->toPlainText();
     QUrl url(QString("https://translate.yandex.net/api/v1.5/tr.json/detect?")
-             + QString("key=") + api_key
+             + QString("key=") + trnsl_api_key
              + QString("&text=") + source_text
              //+ QString("&hint=") + <list of probable text languages>
-             //+ QString("&callback=") + <name of the callback function>
              );
 
     QNetworkRequest request(url);
@@ -132,13 +141,12 @@ void YandexTranslate::translate()
 
     // Create a request.
     QUrl url(QString("https://translate.yandex.net/api/v1.5/tr.json/translate?")
-             + QString("key=") + api_key
+             + QString("key=") + trnsl_api_key
              + QString("&lang=")
              + languages.key(source_lang) + "-" + languages.key(target_lang)
              + QString("&text=") + ui->SourceEdit->toPlainText()
              //+ QString("&format=") + <text format>
              //+ QString("&options=") + <translation options>
-             //+ QString("&callback=") + <name of the callback function>
              );
 
     QNetworkRequest request(url);
@@ -174,7 +182,7 @@ void YandexTranslate::translate()
         return;
     }
 
-    //QString dir = obj.value("lang").toString();   // Use it if you need it.
+    //QString dir = obj.value("lang").toString();   // Use the auto-defined translation direction if you need it.
 
     // Show the translated text.
     QJsonArray text = obj.value("text").toArray();
@@ -182,5 +190,211 @@ void YandexTranslate::translate()
     for(QJsonValue v : text)
     {
         ui->TargetEdit->append(v.toString());
+    }
+}
+
+void YandexTranslate::getTranslationDirections()
+{
+    // Create a request.
+    QUrl url(QString("https://dictionary.yandex.net/api/v1/dicservice.json/getLangs?")
+             + QString("key=") + dict_api_key
+             );
+
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+    // Send the request.
+    QEventLoop loop;
+    QNetworkAccessManager manager;
+    connect(&manager, &QNetworkAccessManager::finished, &loop, &QEventLoop::quit);
+
+    // Receive a reply.
+    QNetworkReply* reply = manager.get(request);
+    loop.exec();
+
+    // Parse the reply.
+    QByteArray bytes = reply->readAll();
+
+    /* A different approach to parsing the reply:
+     * you can check a type of the reply data
+     * in order to choose an appropriate strategy for the parsing process.
+     */
+    QJsonDocument doc = QJsonDocument::fromJson(bytes);
+    if (doc.isObject())
+    {
+        QJsonObject obj = doc.object();
+        // Check the response codes.
+        if(obj.contains("code") || obj.contains("message"))
+        {
+            QMessageBox::information(this,QString::number(obj.value("code").toDouble()),obj.value("message").toString());
+            return;
+        }
+    }
+    else if(doc.isArray())
+    {
+        QJsonArray dirs = doc.array();
+        for(QJsonValue v : dirs)
+        {
+            directions << v.toString();
+        }
+    }
+}
+
+void YandexTranslate::getDictionaryEntry()
+{
+    QString source_lang = ui->comboBoxSource->currentText();
+    QString target_lang = ui->comboBoxTarget->currentText();
+
+    QString dict_dir = languages.key(source_lang) + "-" + languages.key(target_lang);
+
+    /* The following condition is not necessary because
+     * Dictionary API returns ERR_LANG_NOT_SUPPORTED code
+     * if the specified translation direction is not supported.
+     * But, anyway, I want to leave it here for educational purposes.
+     */
+    if(!directions.contains(dict_dir))
+    {
+        QMessageBox::information(this,"501","The specified translation direction is not supported.");
+        return;
+    }
+    else    // else is unnecessary here but I leave it be.
+    {
+        // Create a request.
+        QUrl url(QString("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?")
+                 + QString("key=") + dict_api_key
+                 + QString("&lang=")
+                 + dict_dir
+                 + QString("&text=") + ui->SourceEdit->toPlainText()
+                 //+ QString("&ui=") + <the language of the user's interface for displaying the dictionary entry>
+                 //+ QString("&flags=") + <search options (bitmask of flags)>
+                 );
+
+        QNetworkRequest request(url);
+        request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+        // Send the request.
+        QEventLoop loop;
+        QNetworkAccessManager manager;
+        connect(&manager, &QNetworkAccessManager::finished, &loop, &QEventLoop::quit);
+
+        // Receive a reply.
+        QNetworkReply* reply = manager.get(request);
+        loop.exec();
+
+        // Parse the reply.
+        QByteArray bytes = reply->readAll();
+
+        QJsonDocument doc = QJsonDocument::fromJson(bytes);\
+        QJsonObject obj = doc.object(); // Keys are ("def", "head")
+
+        // Check the response codes.
+        if(obj.contains("code") || obj.contains("message"))
+        {
+            /* In fact, it seems like Dictionary API never returns
+             * ERR_OK code, i.e 200. This is strange because
+             * the code is yet mentioned in the documentation.
+             */
+            QMessageBox::information(this,QString::number(obj.value("code").toDouble()),obj.value("message").toString());
+            return;
+        }
+
+        //QJsonObject head = obj.value("head").toObject();    // Result header (not used).
+
+        QJsonArray def = obj.value("def").toArray();
+        for (QJsonValue v : def)
+        {
+            QJsonObject o = v.toObject();
+            qDebug() << "First level def: " << o.keys();
+            if(o.contains("tr"))
+            {
+                qDebug() << "tr";
+                QJsonArray tr = o.value("tr").toArray();
+                for (QJsonValue val : tr)
+                {
+                    QJsonObject obj = val.toObject();
+                    qDebug() << "Second level tr: " << obj.keys();
+                    if(obj.contains("text"))
+                    {
+                        qDebug() << "text";
+                        qDebug() << obj.value("text").toString();
+                    }
+                    if(obj.contains("pos"))
+                    {
+                        qDebug() << "pos";
+                        qDebug() << obj.value("pos").toString();
+                    }
+                    if(obj.contains("mean"))
+                    {
+                        qDebug() << "mean";
+                        QJsonArray mean = obj.value("mean").toArray();
+                        for(QJsonValue value : mean)
+                        {
+                            qDebug() << value.toObject().value("text").toString();
+                        }
+                    }
+                    if(obj.contains("gen"))
+                    {
+                        qDebug() << "gen";
+                        qDebug() << obj.value("gen").toString();
+                    }
+                    if(obj.contains("syn"))
+                    {
+                        qDebug() << "syn";
+                        QJsonArray syn = obj.value("syn").toArray();
+                        for(QJsonValue value : syn)
+                        {
+                            qDebug() << value.toObject().value("text").toString();
+                        }
+                    }
+                    if(obj.contains("ex"))
+                    {
+                        qDebug() << "ex";
+                        QJsonArray ex = obj.value("ex").toArray();
+                        for(QJsonValue value : ex)
+                        {
+                            QJsonObject object = value.toObject();
+                            qDebug() << object.keys();
+                            qDebug() << "Third level ex: ";
+                        }
+                    }
+                    if(obj.contains("asp"))
+                    {
+                        qDebug() << "asp";
+                        qDebug() << obj.value("asp").toString();    //
+                    }
+                }
+            }
+            if(o.contains("text"))
+            {
+                qDebug() << "text";
+                qDebug() << o.value("text").toString();
+
+            }
+            if(o.contains("pos"))
+            {
+                qDebug() << "pos";
+                qDebug() << o.value("pos").toString();
+            }
+            if(o.contains("ts"))
+            {
+                qDebug() << "ts";
+                qDebug() << o.value("ts").toString();
+            }
+            //qDebug() << v.isArray() << v.isBool() << v.isDouble() << v.isNull() << v.isObject() << v.isString() << v.isUndefined();
+            //qDebug() << v.toObject().contains("tr") << v.toObject().contains("text") << v.toObject().contains("pos");
+            //qDebug() << v.toObject().contains("ex") << v.toObject().contains("syn")  << v.toObject().contains("mean");
+        }
+
+        //qDebug() << def.size();
+
+        /*
+        // Show the dictionary entry.
+        QJsonArray text = obj.value("text").toArray();
+        ui->TargetEdit->clear();
+        for(QJsonValue v : text)
+        {
+            ui->TargetEdit->append(v.toString());
+        }
+        */
     }
 }
--- yandextranslate.h
+++ yandextranslate.h
@@ -17,18 +17,24 @@ public:
     explicit YandexTranslate(QWidget *parent = 0);
     ~YandexTranslate();
 
-    void getSupportedLanguages();   // Get the list of supported languages and show them in comboBoxes.
+    void getSupportedLanguages();   // Get the list of supported languages from Translate API and show them in comboBoxes.
+    void getTranslationDirections();// Get a list of translation directions from Dictionary API.
 
 public slots:
     void detectLanguage();  // Detect a source language and show the result in a corresponding comboBox.
-    void translate();   // Translate some text and show the result.
+    void translate();       // Translate some text and show the result.
 
+    void getDictionaryEntry();  // Get an automatically generated dictionary entry for a word or phrase.
 private:
     Ui::YandexTranslate *ui;
 
-    QString api_key = "";    // Put your API key here.
+    // Translate API
+    QString trnsl_api_key = "";    // Put your Translate API key here.
     QMap<QString, QString> languages; // Supported languages are values, their codes are keys.
-    //QStringList directions; // Deprecated: Translation directions.
+
+    // Dictionary API
+    QString dict_api_key = "";   // Put your Dictionary API key here.
+    QStringList directions; // Translation directions.
 };
 
 #endif // YANDEXTRANSLATE_H
